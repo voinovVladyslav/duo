1428469dfa5c419bb52d2ac2f7cd4bb3221a6297
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-01-11 17:17:12 +0100

    feat: install and configure pydantic settings

---
+++ services/api/config.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+from pydantic import Field, IPvAnyAddress, PostgresDsn, SecretStr, TypeAdapter
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_PATH = Path(__file__).parent
+
+
+class BaseDuoSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=BASE_PATH / '.env',
+        extra='ignore',
+    )
+
+
+class DatabaseSettings(BaseDuoSettings):
+    name: str = Field(alias='postgres_db')
+    host: IPvAnyAddress = Field(alias='postgres_host')
+    port: int = Field(alias='postgres_port')
+    user: str = Field(alias='postgres_user')
+    password: SecretStr = Field(alias='postgres_password')
+
+    @property
+    def dsn(self) -> PostgresDsn:
+        dsn_str = (
+            f'postgresql+psycopg2://'
+            f'{self.user}:{self.password.get_secret_value()}@'
+            f'{self.host}:{self.port}/{self.name}'
+        )
+        return TypeAdapter(PostgresDsn).validate_strings(dsn_str)
+
+
+class Settings(BaseDuoSettings):
+    debug: bool = Field(alias='duo_debug')
+    secret_key: SecretStr = Field(alias='duo_secret_key')
+
+    db: DatabaseSettings = DatabaseSettings()  # type: ignore
+
+settings = Settings() # type: ignore
--- services/api/main.py
+++ services/api/main.py
@@ -1,8 +1,11 @@
+from typing import Any
 from fastapi import FastAPI
+
+from .config import settings
 
 app = FastAPI()
 
 
 @app.get('/')
-def main() -> dict[str, str]:
-    return {'message': 'Okay'}
+def main() -> dict[str, Any]:
+    return {'message': 'Okay', 'settings': settings.model_dump()}
