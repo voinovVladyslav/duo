3ddda360ad28ddbd96936160a2ecd5e9e1c6878c
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-06-16 16:32:00 +0200

    refactor(battleships): split tests into separate files

---
+++ services/game/tests/unit/battleships/test_is_placement_possible.py
@@ -0,0 +1,46 @@
+import pytest
+
+from services.game.engines.battleships import (
+    Coordinate,
+    Direction,
+    is_placement_possible,
+)
+
+
+@pytest.mark.parametrize(
+    ('start', 'direction', 'size', 'result'),
+    [
+        ((0, 0), Direction.RIGHT, 5, True),
+        ((0, 0), Direction.BOTTOM, 5, True),
+        ((0, 0), Direction.LEFT, 5, False),
+        ((0, 0), Direction.TOP, 5, False),
+        ((9, 9), Direction.RIGHT, 5, False),
+        ((9, 9), Direction.BOTTOM, 5, False),
+        ((9, 9), Direction.LEFT, 5, True),
+        ((9, 9), Direction.TOP, 5, True),
+        ((4, 4), Direction.TOP, 2, True),
+        ((4, 4), Direction.LEFT, 2, True),
+        ((4, 4), Direction.RIGHT, 2, True),
+        ((4, 4), Direction.BOTTOM, 2, True),
+        ((1, 1), Direction.BOTTOM, 2, True),
+        ((1, 1), Direction.LEFT, 2, True),
+        ((1, 1), Direction.RIGHT, 2, True),
+        ((1, 1), Direction.TOP, 2, True),
+        ((1, 1), Direction.BOTTOM, 3, True),
+        ((1, 1), Direction.LEFT, 3, False),
+        ((1, 1), Direction.RIGHT, 3, True),
+        ((1, 1), Direction.TOP, 3, False),
+        ((8, 8), Direction.BOTTOM, 2, True),
+        ((8, 8), Direction.LEFT, 2, True),
+        ((8, 8), Direction.RIGHT, 2, True),
+        ((8, 8), Direction.TOP, 2, True),
+        ((8, 8), Direction.BOTTOM, 3, False),
+        ((8, 8), Direction.LEFT, 3, True),
+        ((8, 8), Direction.RIGHT, 3, False),
+        ((8, 8), Direction.TOP, 3, True),
+    ],
+)
+def test_is_placement_possible(
+    start: Coordinate, direction: Direction, size: int, result: bool
+):
+    assert is_placement_possible(start, direction, size) is result
