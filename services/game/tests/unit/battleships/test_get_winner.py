18259ff882fdc9f2c12a1aee08cc3b17987af7b2
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-06-16 16:25:30 +0200

    fix(battleships): correcly decide the winner

--- services/game/tests/unit/battleships/test_get_winner.py
+++ services/game/tests/unit/battleships/test_get_winner.py
@@ -9,8 +9,8 @@ from services.game.engines.battleships import (
 def test_get_winner_return_none_if_both_have_ships():
     g1 = make_empty_grid()
     g2 = make_empty_grid()
-    g1[0][0] = Cell.BOAT
-    g2[0][0] = Cell.BOAT
+    g1[5][5] = Cell.BOAT
+    g2[5][5] = Cell.BOAT
     engine = Battleships(
         state=State(
             current_player=1,
@@ -27,8 +27,8 @@ def test_get_winner_return_none_if_both_have_ships():
 def test_get_winner_return_opponent_if_no_boats():
     g1 = make_empty_grid()
     g2 = make_empty_grid()
-    g1[0][0] = Cell.HIT
-    g2[0][0] = Cell.BOAT
+    g1[5][5] = Cell.HIT
+    g2[5][5] = Cell.BOAT
     engine = Battleships(
         state=State(
             current_player=1,
@@ -45,8 +45,8 @@ def test_get_winner_return_opponent_if_no_boats():
 def test_get_winner_return_opponent_if_no_boats_vice_versa():
     g1 = make_empty_grid()
     g2 = make_empty_grid()
-    g1[0][0] = Cell.BOAT
-    g2[0][0] = Cell.HIT
+    g1[5][5] = Cell.BOAT
+    g2[5][5] = Cell.HIT
     engine = Battleships(
         state=State(
             current_player=1,
