985dc6537a75ae1b6d26ad124fd3a5a382d67336
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-06-16 15:02:52 +0200

    test(battleships): calculate_boat_position

--- services/game/tests/unit/battleships/test_board_generator.py
+++ services/game/tests/unit/battleships/test_board_generator.py
@@ -3,6 +3,7 @@ import pytest
 from services.game.engines.battleships import (
     Coordinate,
     Direction,
+    calculate_boat_position,
     get_coordinates_around_coordinate,
     is_placement_possible,
 )
@@ -81,3 +82,25 @@ def test_get_coordinates_around_coordinate(
     result: set[Coordinate],
 ):
     assert get_coordinates_around_coordinate(coordinate) == result
+
+
+@pytest.mark.parametrize(
+    ('start', 'direction', 'size', 'result'),
+    [
+        ((0, 0), Direction.RIGHT, 2, [(0, 0), (0, 1)]),
+        ((0, 0), Direction.BOTTOM, 2, [(0, 0), (1, 0)]),
+        ((0, 0), Direction.RIGHT, 5, [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)]),
+        ((0, 0), Direction.BOTTOM, 5, [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)]),
+        ((9, 9), Direction.TOP, 2, [(9, 9), (8, 9)]),
+        ((9, 9), Direction.LEFT, 2, [(9, 9), (9, 8)]),
+        ((9, 9), Direction.TOP, 5, [(9, 9), (8, 9), (7, 9), (6, 9), (5, 9)]),
+        ((9, 9), Direction.LEFT, 5, [(9, 9), (9, 8), (9, 7), (9, 6), (9, 5)]),
+    ],
+)
+def test_calculate_boat_position(
+    start: Coordinate,
+    direction: Direction,
+    size: int,
+    result: list[Coordinate],
+):
+    assert calculate_boat_position(start, direction, size) == result
