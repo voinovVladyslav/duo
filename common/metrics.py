253fb2f53b422ad7795a95204e98a764965fa711
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-06-19 21:57:29 +0200

    feat(logs): naive implementation to export logs to loki

--- common/metrics.py
+++ common/metrics.py
@@ -1,9 +1,15 @@
-from opentelemetry import metrics
+import logging
+
+from opentelemetry import _logs, metrics
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import (
     OTLPMetricExporter,
 )
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.sdk.resources import Resource
 
 
 def setup_metrics(
@@ -19,3 +25,21 @@ def setup_metrics(
     provider = MeterProvider(metric_readers=[reader])
     metrics.set_meter_provider(provider)
     return metrics.get_meter(service_name)
+
+
+def setup_logs(
+    service_name: str,
+    endpoint: str,
+    export_interval_millis: int = 5_000,
+) -> None:
+    resource = Resource.create({'service.name': service_name})
+    provider = LoggerProvider(resource=resource)
+    provider.add_log_record_processor(
+        BatchLogRecordProcessor(
+            OTLPLogExporter(endpoint=endpoint, insecure=True),
+            schedule_delay_millis=export_interval_millis,
+        )
+    )
+    _logs.set_logger_provider(provider)
+    handler = LoggingHandler()
+    logging.getLogger().addHandler(handler)
