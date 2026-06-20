41384e6eae3be5cdb7886d0520c147fd60760843
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-06-20 17:01:03 +0200

    feat(logs): export logs to loki for other services
    ignore uvicorn logs (access logs)

--- services/game/__init__.py
+++ services/game/__init__.py
@@ -1,3 +1,6 @@
 from common.logging.main import setup_logging
+from common.metrics import setup_logs
+from services.game.config import settings
 
 setup_logging()
+setup_logs(settings.service_name, settings.otel_url, settings.otel_interval)
